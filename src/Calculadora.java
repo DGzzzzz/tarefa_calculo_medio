72aa877a59b2285fe21d16f951766949e338f8c6
Douglas Winter <douglaswinter75@gmail.com>
2024-02-08 16:02:58 -0300

    First commit

---
+++ src/Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+    public static void main(String args[]) {
+        int nota1 = 10;
+        int nota2 = 8;
+        int nota3 = 9;
+        int nota4 = 7;
+
+        int res = (nota1 + nota2 + nota3 + nota4) / 3;
+
+        System.out.println("Média = " + res);
+    }
+}
